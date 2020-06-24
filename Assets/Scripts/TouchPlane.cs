2493fb5e6e4942b541c45b1faa20dc9a51441f54
Mach Kobayashi <machk@google.com>
2020-06-24 15:17:16 -0700

    added canvas touch panel

---
+++ Assets/Scripts/TouchPlane.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchPlane : UIBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting touch plane");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log($"position: {eventData.position}");
+    }
+}
