9224974be94c38023c7949416eb5ef44fd493cca
Mach Kobayashi <machk@google.com>
2020-06-24 16:21:15 -0700

    starting interactive graph class

---
+++ Assets/Scripts/InteractiveGraph.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveGraph : MonoBehaviour
+{
+    [SerializeField] private GameObject m_Background;
+    [SerializeField] private GameObject m_KnotPrefab;
+
+    private const int kKnotNum = 100;
+    private GameObject[] m_Knots = new GameObject[kKnotNum];
+    private float m_Left;
+    private float m_Width;
+    private float m_Bottom;
+    private float m_Height;
+
+    public void OnDrag(Vector2 pos)
+    {
+        Debug.Log($"{name} getting {pos}");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var backgroundPos = m_Background.transform.localPosition;
+        var backgroundScale = m_Background.transform.localScale;
+        m_Left = backgroundPos.x - 0.5f * backgroundScale.x;
+        m_Width = backgroundScale.x;
+        m_Bottom = backgroundPos.y - 0.5f * backgroundScale.y;
+        m_Height = backgroundScale.y;
+
+        for (int i = 0; i < kKnotNum; i++)
+        {
+            var knot = Instantiate(m_KnotPrefab, transform);
+            knot.transform.localPosition = new Vector3(
+                m_Left + i * m_Width / (kKnotNum - 1),
+                m_Bottom + i * m_Height / (kKnotNum - 1),
+                0);
+            m_Knots[i] = knot;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/TouchPlane.cs
+++ Assets/Scripts/TouchPlane.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class TouchPlane : UIBehaviour, IPointerClickHandler
+public class TouchPlane : UIBehaviour, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Starting touch plane");
-    }
+    [SerializeField] private Camera m_MainCam;
+    [SerializeField] private InteractiveGraph m_LeftGraph;
+    [SerializeField] private InteractiveGraph m_RightGraph;
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log($"position: {eventData.position}");
+        Vector2 pos = new Vector2(
+            m_MainCam.orthographicSize * (eventData.position.x / Screen.width - 0.5f),
+            -m_MainCam.orthographicSize * (eventData.position.y / Screen.height - 0.5f));
+        if (pos.x < 0)
+        {
+            m_LeftGraph.OnDrag(pos);
+        }
+        else
+        {
+            m_RightGraph.OnDrag(pos);
+        }
     }
 }
